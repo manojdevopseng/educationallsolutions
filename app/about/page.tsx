3de7fdfb1583994e17291031c7ffbdd17f8926c3
educationallsolutions <educationallsolutions@gmail.com>
2024-02-18 13:53:03 +0530

    added changes in about page and added classes in navbar to bg-teal-600

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <main>
+      <div>
+        <div className='text-center text-4xl py-24 bg-orange-200'>
+
+        </div>
+      </div>
+      <div className='text-center text-4xl py-7'>
+        <h1 className='text-4xl font-bold'>Welcome to Education All Solutions</h1>
+      </div>
+    </main>
   )
 }
 
