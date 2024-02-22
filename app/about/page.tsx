7b2cc859c616299eae81e1c2fbfef06b90772ec6
manojdevopseng <kumar.manoj8527387747>
2024-02-22 15:16:29 +0530

    added major changes in some files

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
+// import React from 'react'
+import Link from "next/link"
+import { Spotlight } from "@/components/ui/Spotlight"
+import { Button } from "@/components/ui/moving-border";
 
-const About = () => {
+function About() {
   return (
-    <main >
-      <div>
-        <div className='text-center text-4xl py-24 bg-orange-200'>
-
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="blue"
+        />
+        <div className="p-4 relative z-10 w-full text-center" >
+            <h1
+            className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-neutral-400"
+            >Welcome to Education All Solutions</h1>
+            <p
+            className="mt-4 font-normal text-base md:text-lg text-black max-w-lg mx-auto"
+            >Education All Solutions is an online platform that helps you to find the right courses for your needs.</p>
+            <div className="mt-4">
+                <Link href={"/courses"}>
+                    <Button
+                    borderRadius="1.75rem"
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                    Explore Courses
+                    </Button>
+                </Link>
+            </div>
         </div>
-      </div>
-      <div className='text-center text-4xl py-7'>
-        <h1 className='text-4xl font-bold'>Welcome to Education All Solutions</h1>
-        <p className='text-sm text-black mt-4 font-light'>
-          Education All Solutions is an online platform that helps you to find the right courses for your needs.
-        </p>
-      </div>
-    </main>
+    </div>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin');
@@ -11,6 +17,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -18,6 +39,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-debug-screens")],
+  plugins: [addVariablesForColors, require("tailwindcss-debug-screens")],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
