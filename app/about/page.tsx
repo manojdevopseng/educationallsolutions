8fb6a8d1794c2686cf5b5fc4dd659923a41239da
manojdevopseng <kumar.manoj8527387747>
2024-02-22 14:17:44 +0530

    added changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-<<<<<<< HEAD
-    <main>
-=======
     <main >
->>>>>>> origin/main
       <div>
         <div className='text-center text-4xl py-24 bg-orange-200'>
 
@@ -14,12 +10,9 @@ const About = () => {
       </div>
       <div className='text-center text-4xl py-7'>
         <h1 className='text-4xl font-bold'>Welcome to Education All Solutions</h1>
-<<<<<<< HEAD
-=======
         <p className='text-sm text-black mt-4 font-light'>
           Education All Solutions is an online platform that helps you to find the right courses for your needs.
         </p>
->>>>>>> origin/main
       </div>
     </main>
   )
