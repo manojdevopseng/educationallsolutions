3619a78f3a816446a963eb2f0670a5fd8d372de7
educationallsolutions <educationallsolutions@gmail.com>
2024-02-24 00:51:23 +0530

    added changes in Footer and TopBar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,30 +1,14 @@
 /** @format */
-"use client";
-
-type NavItem = {
-  label: string;
-};
-
-const navItems: NavItem[] = [
-  {
-    label: 'Contact +91 9871023007',
-  },
-  {
-    label: 'Write Us-eduallsolutions@gmail.com',
-  },
-  {
-    label: 'Email-info@eduallsolutions.com',
-  }
-];
 
 export default function TopBar() {
   return (
-    <div className="firefox:bg-opacity-90 mx-auto border-b flex bg-black text-white w-full max-w-8xl justify-between px-4 py-1 text-lg">
+    <div className="firefox:bg-opacity-90 mx-auto border-b flex bg-black text-white w-full max-w-8xl 
+      justify-between px-4 py-1 text-lg">
       <section className="flex items-center gap-10">
         <div className="hidden md:flex items-center gap-4 transition-all">
-          {navItems.map((d, i) => (
-            <span>{d.label}</span>
-          ))}
+          <span>Contact +91 9871023007</span>
+          <span>Write Us-eduallsolutions@gmail.com</span>
+          <span>Email-info@eduallsolutions.com</span>
         </div>
       </section>
     </div>
