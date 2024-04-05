652831ab7703f21ea714b24f8671d06d308ea1ae
manojdevopseng <kumar.manoj8527387747>
2024-04-05 17:12:51 +0530

    added a changes and creation some json file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,19 @@
-import Link from "next/link"
-import { Spotlight } from "@/components/ui/Spotlight"
+import Link from "next/link";
+import { Spotlight } from "@/components/ui/Spotlight";
 import { Button } from "@/components/ui/moving-border";
-
 
 function About() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <Spotlight
-        className="top-40 left-0 md:left-60 md:-top-20"
-        fill="blue"
-      />
-      <div className="p-4 z-10 w-auto h-auto text-center" >
-        <h1
-          className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-neutral-400"
-        >About Us</h1>
-        <p
-          className="mt-4 font-normal text-base md:text-lg text-black max-w-lg mx-auto"
-        >Education All Solutions is an online platform that helps you to find the right courses for your needs.</p>
+    <div className="py-12 text-black">
+      <Spotlight className="top-40 left-0 md:left-60 md:-top-20" fill="blue" />
+      <div className="p-4 z-10 w-auto h-auto text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-black to-neutral-400">
+          About Us
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-black max-w-lg mx-auto">
+          Education All Solutions is an online platform that helps you to find
+          the right courses for your needs.
+        </p>
         <div className="mt-4">
           <Link href={"/courses"}>
             <Button
@@ -29,7 +26,7 @@ function About() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- app/courses/cuet-ug/page.tsx
+++ app/courses/cuet-ug/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { Spotlight } from "@/components/ui/Spotlight";
+import courseDataForIner from "@/data/coursesDataForIner.json";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { Url } from "url";
 
-const CuetUG = () => {
-  return (
-    <div>CuetUG</div>
-  )
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
 }
 
-export default CuetUG
+const CuetUg = () => {
+  const featuredCourses = courseDataForIner.courses.filter(
+    (course: Course) => course
+  );
+
+  return (
+    <div className=" bg-gray-200 min-h-screen justify-center item-center py-12 text-black">
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="blue" />
+      <div className="ml-4">
+        <h2 className=" text-3xl text-teal-600 font-semibold tracking-wide uppercase">
+          cuet-ug
+        </h2>
+        <p className="mt-2 text-xl leading-8 font-medium tracking-tight sm:text-4xl uppercase">
+          Details
+        </p>
+      </div>
+      <div className="mt-4 ml-4 mr-4">
+        <div>
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="course">
+              {/* <img src={course.image} alt={course.title} /> */}
+              <h2>{course.title}</h2>
+              <p>
+                <strong>Instructor:</strong> {course.instructor}
+              </p>
+              <p>
+                <strong>Price:</strong> ${course.price}
+              </p>
+              <p>{course.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CuetUg;
