4e6a7a8c822343c63c23ec31a7acbf8fd956c669
manojdevopseng <kumar.manoj8527387747@gmail.com>
2024-05-29 01:49:03 +0530

    updated featurebranch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,12 +9,8 @@ const Dashboard = async () => {
   //   redirect("/");
   // }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <div>
-          <DashNavbar />
-        </div>
-      </div>
+    <div className="">
+      <DashNavbar />
     </div>
   );
 };
